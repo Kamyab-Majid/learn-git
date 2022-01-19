13c1c3e0991c4b9997cf02444f358b1770182480
adrian (majid <majid.kamyab@altaml.com>
2022-01-19 10:30:46 -0700

    feat: adds addition funciton

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 def multiply(num1,num2, num3):
     return num1*num2*3
 
-def add_(num1,num2):
-    return num1+num2
-
-def subtract(n1, n2):
-    return n1-n2 
+def add(num1, num2):
+	return num1 + num2
